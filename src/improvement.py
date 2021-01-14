6083d2c844286665e30c1f6ec0ac9ea2523da195
sarambl <samebr@gmail.com>
2021-01-14 11:21:44 +0100

    finished step E

--- src/improvement.py
+++ src/improvement.py
@@ -1,39 +1,30 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-def plot_temp(temperatures):
 
-
-    # plot results
-    plt.plot(temperatures, 'r-')
+def plot_data(data, xlabel, ylabel):
+    plt.plot(data, 'r-')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.axhline(y=mean, color='b', linestyle='--')
-
-    # visuals for plot:
-    plt.xlabel('Time [h]')
-    plt.ylabel('Temperature [$^\circ$C]')
-
     plt.savefig(str(num_measurements)+'.png')
-
-    #plt.clf()
     plt.show()
+
+
+def compute_statistics(data):
+    mean = sum(data)/num_measurements
+    return mean
+
+
+def read_data(file_name, column):
+    data = pd.read_csv(file_name, nrows=num_measurements)
+    return data[column]
 
 
 for num_measurements in [25, 100, 500]:
 
+    temperatures = read_data(file_name='data/temperatures.csv', column='Air temperature (degC)')
 
-    # read data from file
-    data = pd.read_csv('data/temperatures.csv', nrows=num_measurements)
-    temperatures = data['Air temperature (degC)']
+    mean = compute_statistics(temperatures)
 
-    # compute statistics
-
-    mean = sum(temperatures)/num_measurements
-
-
-    # plot results
-    plot_temp(temperatures)
-
-    #plt.clf()
-    plt.show()
-# %%
-
+    plot_data(data=temperatures, xlabel='measurements', ylabel='air temperature (deg C)')
